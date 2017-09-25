6282c058aa2086c0e046085b13b23265084e065a
nedhinpp <nedhinpp@gmail.com>
2017-09-25 11:38:04 +0530

    C

---
+++ app/src/main/java/com/decoder/barcodedetect/MainActivity.java
@@ -0,0 +1,50 @@
+package com.decoder.barcodedetect;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView barcodeResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        barcodeResult = (TextView) findViewById(R.id.barcode_result);
+
+    }
+
+    public void scanBarcode(View v)
+    {
+       Intent intent = new Intent(this, ScanBarcodeActivity.class);
+        startActivityForResult(intent, 0);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    barcodeResult.setText("Barcode value  : " + barcode.displayValue);
+                } else {
+                    barcodeResult.setText("No Barcode found");
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+}
